b1382c4675767b7aaeecaa1ff0fc2f958edc867b
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-18 12:07:55 -0500

    Gemfile run bundle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
+
+has_secure_password #validations for all the password stuff
 has_many :posts, dependent: :destroy
-validates :username, presence: true
-has_secure_password
+validates :username, presence: true, uniqueness: true
+
 
 ## Remove the uniqueness validation so I can re-run `api-runner.rb` while testing.
 # validates :username, uniqueness: true
