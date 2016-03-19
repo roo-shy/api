87b9aadc6c5de9ba887ff3dd48edd852676ee8f6
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-18 23:32:53 -0500

    Creating a Post API

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   namespace :api do
     get "posts" => 'posts#index'
     get "posts/:id" => 'posts#show'
+    post "posts" => 'posts#create'
+    patch "posts/:id" => 'posts#update'
+    delete "posts/:id" => 'posts#delete'
     post "registrations" => 'registrations#create'
   end
 end
