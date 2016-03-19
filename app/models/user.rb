a6864c5a64445e18eb13f232cdd476fed28a7e31
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-19 13:36:02 -0500

    Fianl Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,6 @@
 class User < ActiveRecord::Base
 
 has_secure_password #validations for all the password stuff
-has_many :posts, dependent: :destroy
 validates :username, presence: true
-
-## Remove the uniqueness validation so I can re-run `api-runner.rb` while testing.
 
 end
