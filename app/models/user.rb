cf05fa2ce4687413dea5dcd3b41f56e5ce22cf3f
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-19 13:05:34 -0500

    Testing API

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ActiveRecord::Base
 
 has_secure_password #validations for all the password stuff
-# has_many :posts, dependent: :destroy
-validates :username, presence: true, uniqueness: true
-# , uniqueness: true
-
+has_many :posts, dependent: :destroy
+validates :username, presence: true
 
 ## Remove the uniqueness validation so I can re-run `api-runner.rb` while testing.
-# validates :username, uniqueness: true
+
 end
